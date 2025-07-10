2dcc2cfd2e68d24ffb9cfb06764fda94d3ef0e2e
amairadadapredisagreement <amairadadapredisagreement@gumote.com>
2025-07-10 08:54:25 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -73,3 +73,7 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+html_css_files = [
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+]
